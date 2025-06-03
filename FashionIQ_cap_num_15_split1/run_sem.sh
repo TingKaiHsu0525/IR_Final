bd72cf828a0e48a6100fbbedf99e2121b9992282
hhhsu-cmlab <hhhsu@cmlab.csie.ntu.edu.tw>
2025-06-03 16:21:28 +0800

    added jsons with 15 captions

---
+++ FashionIQ_cap_num_15_split1/run_sem.sh
@@ -0,0 +1,7 @@
+python src/semantic_editing_search_copy.py \
+  --dataset               fashioniq \
+  --dataset_path          FashionIQ_cap_num_15_split1 \
+  --gpt_version           gpt-3.5 \
+  --submission_name       "cap_num_15_split1" \
+  --caption_type          opt \
+  --nums_caption          7
